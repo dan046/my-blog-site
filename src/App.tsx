14ecb5ed31d66c031fa5ef3c30913a3005fc8b66
IchirouTakashima <ichiroutakashimakun@gmail.com>
2023-05-22 21:07:25 +0800

    testing features

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import { produce } from "immer"
 import AddPost from "./components/AddPost"
 import BlogPost from "./components/BlogPost"
 import Card from "./components/Card"
 import { sampleData } from "./sampleData"
 
 const App = () => {
+  const newData = {
+    title: "New Course",
+    author: "John Doe",
+    date: "2023-06-01",
+    content: "This is a new course.",
+  }
+
+  const updatedSampleData = produce(
+    sampleData,
+    (
+      draft: { title: string; author: string; date: string; content: string }[]
+    ) => {
+      draft.push(newData)
+    }
+  )
+
   return (
     <Card>
-      <AddPost/>
-      <BlogPost post={sampleData} />
+      <AddPost />
+      <BlogPost post={updatedSampleData} />
     </Card>
   )
 }
